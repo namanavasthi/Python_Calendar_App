a2e6c4b7d69240344dd144a6469f48295f2c68a9
NAMAN AVASTHI <naman.avasthi@gmail.com>
2017-09-24 02:44:30 -0700

    working GUI for input

--- calendar_app.py
+++ calendar_app.py
@@ -1,11 +1,84 @@
 import argparse
 from utils import xl_util
 
+from tkinter import *
+
+__author__ = "NAMAN AVASTHI"
+
+root = Tk()
+
+root.geometry("600x400")
+
+root.title("Naman Avasthi iCal APP")
+
+label1 = Label(root, text="Excel File Location : ")
+E1 = Entry(root, bd=2)
+
+label2 = Label(root, text="Name to Search : ")
+E2 = Entry(root, bd=2)
+
+label3 = Label(root, text="Mail address : ")
+E3 = Entry(root, bd=2)
+
+label4 = Label(root, text="Where To Save Path : ")
+E4 = Entry(root, bd=2)
+
+
+def quit_program():
+    root.destroy()
+    root.quit()
+
+
+def destry_ui_components():
+    E1.destroy()
+    E2.destroy()
+    E3.destroy()
+    E4.destroy()
+    label1.destroy()
+    label2.destroy()
+    label3.destroy()
+    label4.destroy()
+    submit.destroy()
+
+
+def get_date():
+    excel_file = E1.get()
+    name_to_find = E2.get()
+    address_to_send = E3.get()
+    path_save_at = E4.get()
+
+    file_return_value = xl_util.open_excel_file(xl_file_path=excel_file, input_name=name_to_find,
+                                                mail_address=address_to_send, save_path=path_save_at)
+
+    if file_return_value == 0:
+        issue = Label(root, text="Something went Wrong! Please Check Inputs Again")
+        issue.pack()
+        resolve = Label(root, text="Contact Developer for possible solutions at navasthi@usc.edu")
+        resolve.pack()
+    else:
+        destry_ui_components()
+        success = Label(root, text="SUCCESS! Calendar Events created at " + path_save_at)
+        success.pack(side=TOP)
+
+
+submit = Button(root, text="Submit", command=get_date)
+exit_button = Button(root, text="Exit", command=quit_program)
+
+label1.grid(row=0,column=0,sticky=W)
+E1.pack()
+label2.pack()
+E2.pack()
+label3.pack()
+E3.pack()
+label4.pack()
+E4.pack()
+submit.pack(side=BOTTOM)
+exit_button.pack(side=BOTTOM)
+root.mainloop()
+
 
 def main(excel_file: str, name_to_find: str, address_to_send: str, path_save_at: str):
     print("this is the excel file : " + excel_file)
-    file_return_value = xl_util.open_excel_file(xl_file_path=excel_file, input_name=name_to_find,
-                                                mail_address=address_to_send, save_path=path_save_at)
 
 
 if __name__ == '__main__':
--- utils/xl_util.py
+++ utils/xl_util.py
@@ -11,102 +11,105 @@ import os
 def open_excel_file(xl_file_path: str, input_name: str, mail_address: str, save_path: str):
     days_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-    xl_file = xl_rd.open_workbook(xl_file_path)
+    try:
+        xl_file = xl_rd.open_workbook(xl_file_path)
 
-    xl_sheet = xl_file.sheet_by_index(0)
+        xl_sheet = xl_file.sheet_by_index(0)
 
-    for row in range(xl_sheet.nrows):
+        for row in range(xl_sheet.nrows):
 
-        # day parameter
-        col = xl_sheet.cell(row, 0).value
+            # day parameter
+            col = xl_sheet.cell(row, 0).value
 
-        if col in days_list:
+            if col in days_list:
 
-            # date parameter in mm/dd/yy format in excel file
-            shift_day = xl_sheet.cell(row, 1).value
-            # convert to python compatible format
-            py_date = xl_rd.xldate.xldate_as_datetime(shift_day, 0)
+                # date parameter in mm/dd/yy format in excel file
+                shift_day = xl_sheet.cell(row, 1).value
+                # convert to python compatible format
+                py_date = xl_rd.xldate.xldate_as_datetime(shift_day, 0)
 
-            for rows_in_day in range(row + 1, xl_sheet.nrows):
+                for rows_in_day in range(row + 1, xl_sheet.nrows):
 
-                # is the time value or day of the week value
-                day_value = xl_sheet.cell(rows_in_day, 0).value
+                    # is the time value or day of the week value
+                    day_value = xl_sheet.cell(rows_in_day, 0).value
 
-                if day_value in days_list:
-                    break
-                else:
-                    #gives the shift position
-                    location_field = xl_sheet.cell(rows_in_day, 1).value
+                    if day_value in days_list:
+                        break
+                    else:
+                        #gives the shift position
+                        location_field = xl_sheet.cell(rows_in_day, 1).value
 
-                    # this for loop is to check names in last 3 columns
-                    for cols in range(3, 6):
+                        # this for loop is to check names in last 3 columns
+                        for cols in range(3, 6):
 
-                        # get those column values and check with given input name
-                        name_field = xl_sheet.cell(rows_in_day, cols).value
+                            # get those column values and check with given input name
+                            name_field = xl_sheet.cell(rows_in_day, cols).value
 
-                        if name_field == input_name:
+                            if name_field == input_name:
 
-                            # manipulation of time value, i.e. HH:MMAM/PM - HH:MMAM/PM
-                            day_value = day_value.split(" - ")
-                            date_start = day_value[0]
-                            date_end = day_value[1]
+                                # manipulation of time value, i.e. HH:MMAM/PM - HH:MMAM/PM
+                                day_value = day_value.split(" - ")
+                                date_start = day_value[0]
+                                date_end = day_value[1]
 
-                            hours_start = date_start.split(":")[0]
-                            hours_check = (date_start.split(":")[1])[2]
-                            if hours_check == 'P':
-                                if hours_start != "12":
-                                    hours_start = str(int(hours_start) + 12)
-                                minutes_start = (date_start.split(":")[1]).split("PM")[0]
-                            else:
-                                minutes_start = (date_start.split(":")[1]).split("AM")[0]
+                                hours_start = date_start.split(":")[0]
+                                hours_check = (date_start.split(":")[1])[2]
+                                if hours_check == 'P':
+                                    if hours_start != "12":
+                                        hours_start = str(int(hours_start) + 12)
+                                    minutes_start = (date_start.split(":")[1]).split("PM")[0]
+                                else:
+                                    minutes_start = (date_start.split(":")[1]).split("AM")[0]
 
-                            hours_end = date_end.split(":")[0]
-                            hours_check = (date_end.split(":")[1])[2]
-                            if hours_check == 'P':
-                                if hours_end != "12":
-                                    hours_end = str(int(hours_end) + 12)
-                                minutes_end = (date_end.split(":")[1]).split("PM")[0]
-                            else:
-                                minutes_end = (date_end.split(":")[1]).split("AM")[0]
-
-
-                            # CALENDAR EVENT CREATION
-
-                            cal = Calendar()
-                            event = Event()
-
-                            la = pytz.timezone("America/Los_Angeles")
-
-                            event.add('summary', 'Lib Sec Shift')
-
-                            event.add('location',location_field)
-
-                            # datetime format = (yyyy,m,day,hrs,mins,secs)
-
-                            event.add('dtstart',
-                                      la.localize(
-                                          datetime.datetime(py_date.year, py_date.month, py_date.day,
-                                                            int(hours_start), int(minutes_start), 0)))
+                                hours_end = date_end.split(":")[0]
+                                hours_check = (date_end.split(":")[1])[2]
+                                if hours_check == 'P':
+                                    if hours_end != "12":
+                                        hours_end = str(int(hours_end) + 12)
+                                    minutes_end = (date_end.split(":")[1]).split("PM")[0]
+                                else:
+                                    minutes_end = (date_end.split(":")[1]).split("AM")[0]
 
 
-                            event.add('dtend',
-                                      la.localize(
-                                          datetime.datetime(py_date.year, py_date.month, py_date.day,
-                                                            int(hours_end), int(minutes_end), 0)))
+                                # CALENDAR EVENT CREATION
 
-                            event.add('dtstamp',
-                                      la.localize(datetime.datetime(py_date.year, py_date.month, py_date.day,
-                                                                    0, 10, 0)))
+                                cal = Calendar()
+                                event = Event()
+
+                                la = pytz.timezone("America/Los_Angeles")
+
+                                event.add('summary', 'Lib Sec Shift')
+
+                                event.add('location',location_field)
+
+                                # datetime format = (yyyy,m,day,hrs,mins,secs)
+
+                                event.add('dtstart',
+                                          la.localize(
+                                              datetime.datetime(py_date.year, py_date.month, py_date.day,
+                                                                int(hours_start), int(minutes_start), 0)))
 
 
-                            cal.add('attendee',mail_address)
+                                event.add('dtend',
+                                          la.localize(
+                                              datetime.datetime(py_date.year, py_date.month, py_date.day,
+                                                                int(hours_end), int(minutes_end), 0)))
 
-                            cal.add_component(event)
+                                event.add('dtstamp',
+                                          la.localize(datetime.datetime(py_date.year, py_date.month, py_date.day,
+                                                                        0, 10, 0)))
 
-                            save_file = str(col) + str(day_value) + ".ics"
 
-                            f = open(os.path.join(save_path, save_file), 'wb')
-                            f.write(cal.to_ical())
-                            f.close()
+                                cal.add('attendee',mail_address)
 
-    return xl_file
+                                cal.add_component(event)
+
+                                save_file = str(col) + str(day_value) + ".ics"
+
+                                f = open(os.path.join(save_path, save_file), 'wb')
+                                f.write(cal.to_ical())
+                                f.close()
+    except FileNotFoundError:
+        return 0
+
+    return 1
